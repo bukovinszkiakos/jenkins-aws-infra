9d79ed33a6993f44c9e77c477c4a0f4be1f8b41e
Akos Bukovinszki <nashyro1995@gmail.com>
2026-05-06 19:09:51 +0200

    Testing actions trigger

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -20,6 +20,7 @@ resource "aws_iam_role_policy_attachment" "this" {
   policy_arn = var.policy_arn
 }
 
+
 resource "aws_iam_instance_profile" "this" {
   name = var.instance_profile_name
   role = aws_iam_role.this.name
